ac64410fb4ba38c94ffda1a43d43b14075fa5f97
Vitor <vitorjuliani99@gmail.com>
2019-11-23 18:29:15 -0300

    insert knn operator and remove empty values

--- script.r
+++ script.r
@@ -1,14 +1,23 @@
 library(ggplot2)
+library(class)
+library(caret)
+library(e1071)
 
 # Importing the dataset
-heartDiseaseDataframe <- read.table("./dataset/processed.cleveland.data", fileEncoding = "UTF-8",sep=",")
+heartDiseaseDataframe <- read.csv("./dataset/processed.cleveland.data", fileEncoding = "UTF-8",sep=",", header = FALSE)
+
+# Setting the name of the columns
 colnames(heartDiseaseDataframe) <- c("Age", "Sex", "ChestPainType", "RestBloodPressure", "SerumCholestoral", "FastingBloodSugar", "ResElectrocardiogr
 aphic", "MaxHeartRate", "ExerciseInduced", "Oldpeak", "Slope", "MajorVessels", "Thal", "Class")
 
+# Change "?" to NA
+heartDiseaseDataframe[heartDiseaseDataframe == "?"] <- NA
+
 # Removing rows with NA values
-na.omit(heartDiseaseDataframe)
+heartDiseaseDataframe <- na.omit(heartDiseaseDataframe)
 
 # Visualization
+
   # Age distribution
   qplot(heartDiseaseDataframe$Age, geom="histogram") +
     labs(x = "Age", y = "count")
@@ -20,3 +29,40 @@ na.omit(heartDiseaseDataframe)
   
   # Relantionship between blood pressure and heart rate
   plot(heartDiseaseDataframe$RestBloodPressure, heartDiseaseDataframe$MaxHeartRate)
+  
+# Data classification
+  
+  # Getting dataframe class
+  classes <- as.factor(heartDiseaseDataframe[,ncol(heartDiseaseDataframe)])
+  
+  # Removing class column
+  data <- heartDiseaseDataframe[, -ncol(heartDiseaseDataframe)]
+  
+  # Selecting data for test and training
+  set.seed(123)
+  
+  sample_size <- floor(0.8 * nrow(data))
+  train_index <- sample(seq_len(nrow(data)), size = sample_size)
+  
+  # Preparing test object and training
+  train <- data[train_index, ]
+  test <- data[-train_index, ]
+  
+  # Selecting the test class column and train class column
+  trainClass <- as.factor(classes[train_index])
+  testClass <- as.factor(classes[-train_index])
+  
+  # KNN classification without pca
+  knn_res1 <- knn(train, test, trainClass, 1)
+  knn_res3 <- knn(train, test, trainClass, 3)
+  knn_res5 <- knn(train, test, trainClass, 5)
+  knn_res7 <- knn(train, test, trainClass, 7)
+  knn_res9 <- knn(train, test, trainClass, 9)
+  
+  # Accuracy of KNN without pca
+  confusionMatrix(knn_res1, testClass)
+  confusionMatrix(knn_res3, testClass)
+  confusionMatrix(knn_res5, testClass)
+  confusionMatrix(knn_res7, testClass)
+  confusionMatrix(knn_res9, testClass)
+  
