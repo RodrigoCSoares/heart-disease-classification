cfc3d4aa5bab7ee7c06db5e47c7275d0a5f0d8da
Vitor <vitorjuliani99@gmail.com>
2019-11-25 23:26:18 -0300

    metodos de classificacao e remocao dos outliers

--- script.r
+++ script.r
@@ -18,10 +18,13 @@ colnames(heartDiseaseDataframe) <- c("Age", "Sex", "ChestPainType", "RestBloodPr
   # Dataset structure
   str(heartDiseaseDataframe)
 
-# Adjusting data
+# Adjusting data - 1
   
   # Change "?" to NA
   heartDiseaseDataframe[heartDiseaseDataframe == "?"] <- NA
+  
+  # Removing NA values
+  heartDiseaseDataframe <- na.omit(heartDiseaseDataframe)
 
 # Handling table columns
   
@@ -29,54 +32,10 @@ colnames(heartDiseaseDataframe) <- c("Age", "Sex", "ChestPainType", "RestBloodPr
   heartDiseaseDataframe$Class <- ifelse(test = heartDiseaseDataframe$Class == 0, yes = "Healthy", no = "Unhealthy")
   heartDiseaseDataframe$Class <- as.factor(heartDiseaseDataframe$Class)
   
-  # Left columns 
-  for(i in 1:length(heartDiseaseDataframe[,-ncol(heartDiseaseDataframe)])){
-    if (colnames(heartDiseaseDataframe[i]) != colnames(heartDiseaseDataframe[10])) {
-      heartDiseaseDataframe[,i] <- as.integer(heartDiseaseDataframe[,i])
-    }
-  }
+  # Other columns
+  heartDiseaseDataframe[,c(1:9,11:13)] <- sapply(heartDiseaseDataframe[,c(1:9,11:13)], as.integer)
   
-# Cleaning data
-  
- # Omiting rows with empty values
- heartDiseaseDataframe <- na.omit(heartDiseaseDataframe)
- 
- # Removing outliers for each column
- for(i in 1:length(heartDiseaseDataframe[,-ncol(heartDiseaseDataframe)])){
-   outlier_values <- boxplot.stats(heartDiseaseDataframe[,i])$out
-   heartDiseaseDataframe <- heartDiseaseDataframe[!(heartDiseaseDataframe[,i] %in% outlier_values), ]
- }
- 
-# Visualization
-  
-  # General visualization
-  plot(heartDiseaseDataframe)
-  
-  # Boxplot for all columns
-  for (i in 1:length(heartDiseaseDataframe)) {
-    boxplot(heartDiseaseDataframe[,i]~Class, data = heartDiseaseDataframe, main = colnames(heartDiseaseDataframe[i]), ylab = colnames(heartDiseaseDataframe[i]))
-    Sys.sleep(2)
-  }
-  
-  # Histogram for all columns
-  for (i in 1:length(heartDiseaseDataframe[,-ncol(heartDiseaseDataframe)])) {
-    hist(heartDiseaseDataframe[,i], main = colnames(heartDiseaseDataframe[i]), xlab = colnames(heartDiseaseDataframe[i]))
-    Sys.sleep(2)
-  }
-  
-  # Age distribution
-  qplot(heartDiseaseDataframe$Age, geom="histogram") +
-    labs(x = "Age", y = "count")
-  
-  # Genre distribution
-  ggplot(heartDiseaseDataframe, aes(Sex)) +
-    geom_bar(fill = "#0073C2FF") +
-    labs(x = "Sex (0 = Female, 1 = Male)")
-  
-  # Relantionship between blood pressure and heart rate
-  plot(heartDiseaseDataframe$RestBloodPressure, heartDiseaseDataframe$MaxHeartRate)
-  
-# Data classification
+# Data classification with all datas
   
   # Getting dataframe class
   classes <- as.factor(heartDiseaseDataframe[,ncol(heartDiseaseDataframe)])
@@ -88,51 +47,68 @@ colnames(heartDiseaseDataframe) <- c("Age", "Sex", "ChestPainType", "RestBloodPr
   train_index <- sample(seq_len(nrow(heartDiseaseDataframe)), size = sample_size)
   
   # Preparing test object and training
-  train <- heartDiseaseDataframe[train_index, -ncol(heartDiseaseDataframe)]
-  train_svm <- heartDiseaseDataframe[train_index,] 
+  train_without_column <- heartDiseaseDataframe[train_index, -ncol(heartDiseaseDataframe)]
+  train <- heartDiseaseDataframe[train_index,] 
   test <- heartDiseaseDataframe[-train_index, -ncol(heartDiseaseDataframe)]
   
   # Selecting the test class column and train class column
-  trainClass <- as.factor(classes[train_index])
-  testClass <- as.factor(classes[-train_index])
+  trainClass <- classes[train_index]
+  testClass <- classes[-train_index]
   
   # KNN classification
-  knn_res <- knn(train, test, trainClass, 1)
+  knn_res <- knn(train_without_column, test, trainClass, 9)
   
   # Accuracy of KNN
-  confusionMatrix(knn_res, testClass)
+  cf_knn <- confusionMatrix(knn_res, testClass)
+  
+  # Add result to dataframe
+  data <- as.data.frame(cf_knn$overall)
   
   # SVM classification
   svm_classifier <- svm(formula = Class ~ .,
-                   data = train_svm,
+                   data = train,
                    type = 'C-classification',
                    kernel = 'linear')
   
   svm_res <- predict(svm_classifier, newdata = test)
   
   # Accuracy of SVM
-  confusionMatrix(svm_res, testClass)
+  cf_svm <- confusionMatrix(svm_res, testClass)
   
-  # NB classification
-  nb_classifier <- train(train, 
-                         trainClass, 
-                         'nb', 
-                         trControl=trainControl(method='cv',number=10))
-  
-  nb_res <- predict(nb_classifier, newdata = test)
-  
-  # Accuracy of NB
-  confusionMatrix(nb_res, testClass)
+  # Add result to dataframe
+  data[,2] <- as.data.frame(cf_svm$overall)
   
   # RF classification
   rf_classifier <- randomForest(formula = Class ~ .,
-                                data = train_svm,
+                                data = train,
                                 ntree = 500,
                                 importance = TRUE)
   
   rf_res = predict(rf_classifier, newdata = test)
   
   # Accuracy of RF
-  confusionMatrix(as.factor(rf_res), testClass)
-
+  cf_rf <- confusionMatrix(as.factor(rf_res), testClass)
   
+  # Add result to dataframe
+  data[,3] <- as.data.frame(cf_rf$overall)
+  
+# Adjusting data - 2
+  
+  # Removing outliers
+  outlier_values <- boxplot.stats(heartDiseaseDataframe[,12])$out
+  heartDiseaseDataframe <- heartDiseaseDataframe[!(heartDiseaseDataframe[,12] %in% outlier_values), ]
+  
+  outlier_values <- boxplot.stats(heartDiseaseDataframe[,10])$out
+  heartDiseaseDataframe <- heartDiseaseDataframe[!(heartDiseaseDataframe[,10] %in% outlier_values), ]
+  
+  outlier_values <- boxplot.stats(heartDiseaseDataframe[,9])$out
+  heartDiseaseDataframe <- heartDiseaseDataframe[!(heartDiseaseDataframe[,9] %in% outlier_values), ]
+  
+  outlier_values <- boxplot.stats(heartDiseaseDataframe[,6])$out
+  heartDiseaseDataframe <- heartDiseaseDataframe[!(heartDiseaseDataframe[,6] %in% outlier_values), ]
+  
+  outlier_values <- boxplot.stats(heartDiseaseDataframe[,5])$out
+  heartDiseaseDataframe <- heartDiseaseDataframe[!(heartDiseaseDataframe[,5] %in% outlier_values), ]
+  
+  outlier_values <- boxplot.stats(heartDiseaseDataframe[,1])$out
+  heartDiseaseDataframe <- heartDiseaseDataframe[!(heartDiseaseDataframe[,1] %in% outlier_values), ]
